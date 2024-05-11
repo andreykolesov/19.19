bd8c7e8cc4e7e7765451f1e85299b093674087f1
andreykolesov <1yasnoclown1@gmail.com>
2024-05-11 22:02:21 +0300

    longestWordInString

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include "libs/algorithms/tasks/convert_float_numbers.h"
 #include "libs/algorithms/tasks/values_of_expressions.h"
 #include "libs/algorithms/tasks/filter_words.h"
+#include "libs/algorithms/tasks/longest_word_in_string.h"
 
 void test() {
     test_matrix_transpose();
     test_convertFloatNumbers();
     test_valuesOfExpressions(); // выводит "unlnown operator" (3 тест)
     test_filterWords();
+    test_longestWordInString();
 }
 
 int main() {
